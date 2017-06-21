68859b7f506ad011d929c2bf0fbaccbde0446df3
Gregg Roemhildt <roemhildtg@gmail.com>
2017-06-20 19:10:37 -0500

    add less for .less files

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,15 @@ module.exports = {
 
   module: {
     rules: [{
+            test: /\.less$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "less-loader" // compiles Less to CSS
+            }]
+        }, {
       test: /\.stache$/,
       use: {
         loader: 'can-stache-loader'
